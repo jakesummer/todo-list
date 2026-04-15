90b8bffd006706b12aaced979a5e84cfa4efb04c
jakesummer <jakersummer@gmail.com>
2026-04-15 16:51:58 -0400

    Add list of projects and getter to appManager

--- src/appManager.js
+++ src/appManager.js
@@ -2,6 +2,12 @@ import Todo from "./todo.js";
 import Project from "./project.js";
 
 export default class AppManager {
+    #projects = [];
+
+    get projects() {
+        return Object.freeze([...this.#projects]);
+    }
+
     createNewTodo(todoTitle, description, dueDate, priority, project) {
         const newTodo = new Todo(todoTitle, description, dueDate, priority);
         project.todoList.push(newTodo);
